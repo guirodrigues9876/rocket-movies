dad7ebfe5e97aac874c1d4662c5ea19818c4822e
guilherme <guilherme121225@gmail.com>
2023-06-14 19:39:16 -0300

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    COLORS: {
+      BACKGROUND_900: "#1C1B1E",
+      BACKGROUND_800: "#262529",
+      BACKGROUND_700: "#3E3B47",
+  
+      WHITE: "#F4EDE8",
+      PINK: "#FF859B",
+  
+      GRAY_100: "#CAC4CF",
+      GRAY_300: "#948F99",
+  
+      BLUE: "#312E38"
+    }
+  }
