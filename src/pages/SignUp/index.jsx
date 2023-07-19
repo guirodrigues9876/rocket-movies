b71005bba840d5e220990221390a3e24ad795771
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-07-18 23:54:07 -0300

    feat: context and hooks

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import { FiMail, FiLock, FiUser} from 'react-icons/fi';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { api } from "../../services/api";
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Container, Form, Background } from './styles'
 
 export function SignUp(){
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    async function handleSignup(){
+        if(!name || !email || !password){
+            return alert("Preencha todos os campos!");
+        }
+
+        api.post("/users", { name, email, password }).then(()=>{
+            alert("Usuário cadastrado com sucesso!");
+            navigate("/");
+        })
+        .catch(error => {
+            if(error.response){
+                alert(error.response.data.message);
+            }else{
+                alert("Não foi possível cadastrar");
+            }
+        });
+    }
+
     return(
         <Container>
             <Form>
@@ -19,19 +46,22 @@ export function SignUp(){
                     icon= {FiUser}
                     type= "text"
                     placeholder="Nome"
+                    onChange={e => setName(e.target.value)}
                     />
                 <Input
                     icon= {FiMail}
                     type= "text"
                     placeholder="E-mail"
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <Input
                     icon= {FiLock}
                     type="password"
                     placeholder="Senha"
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Cadastrar"/>
+                <Button title="Cadastrar" onClick={handleSignup} />
 
                 <Link to="/">
                     Voltar para o login
